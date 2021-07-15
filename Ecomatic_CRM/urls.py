38cb19074de2650bf6cb200d2a4ab3ab0df0b47b
Abdelrahman Fahmy <rooted39@gmail.com>
2021-07-16 01:57:37 +0200

    pre-release commit

--- Ecomatic_CRM/urls.py
+++ Ecomatic_CRM/urls.py
@@ -34,3 +34,5 @@ urlpatterns = [
     path("Tickets/", include("Tickets.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Ecomatic-CRM Adminstration"
