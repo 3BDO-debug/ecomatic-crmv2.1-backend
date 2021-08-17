457006b6b8567d6e470f74f7a9e56d1a359e193a
Abdelrahman Fahmy <rooted39@gmail.com>
2021-08-17 11:33:29 +0200

    trying config vercel domains

--- Ecomatic_CRM/settings.py
+++ Ecomatic_CRM/settings.py
@@ -91,6 +91,25 @@ SIMPLE_JWT = {
     "TOKEN_TYPE_CLAIM": "token_type",
 }
 
+
+CORS_ALLOW_ALL_ORIGINS = (
+    True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+)
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3030",
+    "https://ecomatic-crm-v2-0-frontend.vercel.app",
+    "https://ecomatic-crm-ar-34a6okvcd-3bdo-debug.vercel.app",
+    "https://ecomatic-crm-v2-0-en-orycdwiav-3bdo-debug.vercel.app",
+]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    "http://localhost:3030",
+    "https://ecomatic-crm-v2-0-frontend.vercel.app",
+    "https://ecomatic-crm-ar-34a6okvcd-3bdo-debug.vercel.app",
+    "https://ecomatic-crm-v2-0-en-orycdwiav-3bdo-debug.vercel.app",
+]
+
+
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "https://ecomatic-crm-v2-0-frontend.vercel.app",
